4556742dd0fd6b215c65378e352cbacc488e1483
DaniAbdulm <daniabdulm@gmail.com>
2024-03-28 22:50:22 -0400

    Added Packet Capturing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from scapy.all import sniff
+from scapy.all import sniff, wrpcap
 import argparse
 
 def main(): 
@@ -6,6 +6,7 @@ def main():
 
     #Adding arguments 
     parser.add_argument('-c', '--capture', help='Capture packets on the network', action='store_true')
+    parser.add_argument('-n', '--num_packets', type=int, default=10, help='Number of packets to capture (default: 10)')
     parser.add_argument('-a', '--analyse', help='Analyse captured packets', action='store_true')
     parser.add_argument('-d', '--discover', help='Discover devices on the network', action='store_true')
     parser.add_argument('-p', '--portscan', type=str, help='Scan ports on a specific IP address')
@@ -13,7 +14,7 @@ def main():
 
     if args.capture:
         #Call packet capturing function 
-        print("Capturing packets...")
+        capture_packets(count=args.num_packets)
 
     if args.analyse: 
         #Call traffic analyses function 
@@ -27,6 +28,22 @@ def main():
         #Call port scanning function with the provided IP address
         print("Scanning ports on IP...")
 
+#function to capture packets
+def capture_packets(count=10, save_to_file=False):
+    print(f"Capturing {count} packets...")
+
+    #capture packets
+    packets = sniff(count=count)
+
+    #print summary of each packet
+    for packet in packets: 
+        print(packet.summary())
+    
+    #Save to file if requested
+    if save_to_file:
+        filename = "captured_packets.pcap"
+        wrpcap(filename, packets)
+        print(f"Captured packets were save to {filename}")
 
 if __name__ == "__main__":
     main()
