8d55a8379ce675ce68f7c70bfc293073af1e889c
DaniAbdulm <daniabdulm@gmail.com>
2024-03-29 15:48:23 -0400

    Added bandwidth and connection frequency analysis feature

--- vultus.py
+++ vultus.py
@@ -1,24 +1,34 @@
 from scapy.all import sniff, wrpcap
 from scapy.all import rdpcap
-from scapy.layers.inet import TCP, UDP, ICMP
+from scapy.layers.inet import IP, TCP, UDP, ICMP
 import argparse
 import time
+from colorama import Fore, Style, init
 from collections import Counter
 
+#initialize colorama 
+init(autoreset=True)
+
 def main(): 
-    parser = argparse.ArgumentParser(description='Vultus Network Analyser 1.1.0')  
+    parser = argparse.ArgumentParser(description= Fore.LIGHTYELLOW_EX + 'Vultus Network Analyser 1.1.0')  
 
     #Adding arguments 
+    #packet capture 
     parser.add_argument('-c', '--capture', help='Capture packets on the network', action='store_true')
     parser.add_argument('-n', '--num_packets', type=int, default=10, help='Number of packets to capture (default: 10)')
+    #file saving
     parser.add_argument('--save', type=str, help='Save the captured packets to a PCAP file')
     parser.add_argument('--load', type=str, help='Load and analyse packets from a PCAP file')
+    #analysis arguments
     parser.add_argument('-ap', '--analyse-protocol', action='store_true', help='Analyse the distribution of protocols')
     parser.add_argument('-att', '--analyse-top-talkers', action='store_true', help='Identify the top talkers')
     parser.add_argument('-ab', '--analyse_bandwidth_usage', action='store_true', help='Analyse the bandwidth usage')
-    parser.add_argument('--duration', type=int, default=10, help='Duration of capture for bandwidth analysis in seconds (default: 10s)')
+    parser.add_argument('--duration', type=int, default=10, help='Duration of capture for bandwidth/connection analysis in seconds (default: 10s)')
     parser.add_argument('--interval', type=int, default=1, help='Interval for bandwidth calculation in seconds (default: 1s)')
+    parser.add_argument('-ac', '--analyse_connections', action='store_true', help='Analyse connection frequencies')
+    #device discovery
     parser.add_argument('-d', '--discover', help='Discover devices on the network', action='store_true')
+    #scanning ports
     parser.add_argument('-p', '--portscan', type=str, help='Scan ports on a specific IP address')
     args = parser.parse_args()
 
@@ -39,16 +49,19 @@ def main():
         if packets is not None:
             analyse_protocol_distribution(packets)
         else:
-            print("No packets loaded for analysis.")
+            print(Fore.RED + "No packets loaded for analysis.")
     
     if args.analyse_top_talkers:
         if packets is not None:
             analyse_top_talkers(packets)
         else:
-            print("No packets loaded for analysis.")
+            print(Fore.RED + "No packets loaded for analysis.")
     
     if args.analyse_bandwidth_usage:
             analyse_bandwidth_usage(file_name=args.load, capture_duration=args.duration, interval=args.interval)
+
+    if args.analyse_connections:
+        analyse_connection_frequency(file_name=args.load, capture_duration=args.duration)
 
     if args.discover:
         #Call device discover analyses function
@@ -60,17 +73,17 @@ def main():
 
 #function to capture packets
 def capture_packets(file_name="captured_packets.pcap", count=10):
-    print(f"Capturing {count} packets...")
+    print(f"{Fore.LIGHTBLACK_EX}Capturing {count} packets...")
 
     #capture packets
     packets = sniff(count=count)
 
     #print summary of each packet
     for packet in packets: 
-        print(packet.summary())
+        print(Fore.GREEN + packet.summary())
 
     wrpcap(file_name, packets)
-    print(f"Captured packets saved to {file_name}")
+    print(f"{Fore.LIGHTYELLOW_EX}Captured packets saved to {file_name}")
 
 #function to load packet file 
 def load_packets(file_name="captured_packets.pcap"):
@@ -89,24 +102,24 @@ def analyse_protocol_distribution(packets):
             protocols.append("ICMP")
 
     distribution = Counter(protocols)
-    print("Protocol Distribution:")
+    print(Fore.LIGHTYELLOW_EX + "Protocol Distribution:")
     for protocol, count in distribution.items(): 
-        print(f"{protocol}: {count}")
+        print(f"{Fore.GREEN}{protocol}: {count}")
 
 #function to identify top talkers 
 def analyse_top_talkers(packets): 
     ips = [packet['IP'].src for packet in packets if packet.haslayer("IP")]
     top_talkers = Counter(ips).most_common(5)
-    print("Top Talkers:")
+    print(Fore.LIGHTYELLOW_EX + "Top Talkers:")
     for ip, count in top_talkers:
-        print(f"{ip}: {count}")
+        print(f"{Fore.GREEN}{ip}: {count}")
 
 def analyse_bandwidth_usage(file_name=None, capture_duration=10, interval=1): 
     if file_name: 
-        print(f"Bandwidth analysis from {file_name}")
+        print(f"{Fore.LIGHTBLACK_EX}Bandwidth analysis from {file_name}:")
         packets = rdpcap(file_name)
     else:
-        print(f"Capturing packets for {capture_duration}s...")
+        print(f"{Fore.LIGHTBLACK_EX}Capturing packets for {capture_duration}s...")
         packets = sniff(timeout=capture_duration)
 
     start_time = time.time()
@@ -137,12 +150,45 @@ def analyse_bandwidth_usage(file_name=None, capture_duration=10, interval=1):
     bytes_per_interval.append((current_interval_start, current_interval_bytes))
 
     #printing out the bandwidth usage
-    print("Bandwidth usage (bytes per interval):")
+    print(Fore.LIGHTYELLOW_EX + "Bandwidth usage (bytes per interval):")
     for interval_start, bytes_count in bytes_per_interval:
-        print(f"Time: {interval_start - start_time:.2f}s, Bytes: {bytes_count}")
+        print(f"{Fore.GREEN}Time: {interval_start - start_time:.2f}s, Bytes: {bytes_count}")
 
-def analyse_connection(packets): 
-    pass
+def analyse_connection_frequency(file_name=None, capture_duration=10): 
+    #capture packets
+    if file_name: 
+        packets = rdpcap(file_name)
+    else: 
+        print(f"{Fore.LIGHTBLACK_EX}Analysing connection frequencies for {capture_duration}s...")
+        packets = sniff(timeout=capture_duration)
+
+    #dictionary that hold connection frequency 
+    connections = Counter()
+
+    #analyse packets
+    for packet in packets: 
+        if packet.haslayer(IP): 
+            if packet.haslayer(TCP) or packet.haslayer(UDP): 
+                #extract packet info base on the transport layer protocol
+                transport_layer = packet[UDP] if packet.haslayer(UDP) else packet[TCP]
+                src_ip = packet[IP].src
+                dst_ip = packet[IP].dst
+                src_port = transport_layer.sport
+                dst_port = transport_layer.dport 
+                protocol = 'UDP' if packet.haslayer(UDP) else 'TCP'
+
+                #creating a tuple that represents the connection
+                connection = (src_ip, dst_ip, src_port, dst_port, protocol)
+
+                #increment the count for this connection 
+                connections[connection] += 1
+    
+    print(Fore.LIGHTYELLOW_EX + "Connection Frequencies (src ip, dst ip, src port, dst port, protocol):")
+    for connection, count in connections.items(): 
+        print(f"{Fore.GREEN}{connection}: {count}")
+
+    return connections #returning the result in case of further processing 
+
 
 if __name__ == "__main__":
     main()
